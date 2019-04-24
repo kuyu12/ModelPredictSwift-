86825b53cd9939baf7e6bf4b5f7fd630905aa6f2
Yair Hadad <kuyu12@gmail.com>
2019-04-24 18:32:28 +0300

    Create base project

---
+++ ModelPredictSwift/ModelPredictSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ModelPredictSwift
+//
+//  Created by yair hadad on 24/04/2019.
+//  Copyright © 2019 yair hadad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
